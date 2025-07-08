f7ebd317e66490cd7267f08273ff35fa1ec1bf95
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2025-07-09 02:20:22 +0300

    refactor grid fragment shader

--- Renderer/Shaders/gridFragment.glsl
+++ Renderer/Shaders/gridFragment.glsl
@@ -8,50 +8,50 @@ out vec4 FragColor;
 uniform mat4 view;
 uniform mat4 projection;
 
-
-vec4 grid(vec3 fragPos3D, float scale, bool drawAxis) {
-    vec2 coord = fragPos3D.xz * scale;
+vec4 grid(vec3 worldPos, float scale, bool drawAxis) {
+    vec2 coord = worldPos.xz * scale;
     vec2 derivative = fwidth(coord);
     vec2 grid = abs(fract(coord - 0.5) - 0.5) / derivative;
-    float line = min(grid.x, grid.y);
-    float minimumz = min(derivative.y, 1);
-    float minimumx = min(derivative.x, 1);
-    vec4 color = vec4(0.2, 0.2, 0.2, 1.0 - min(line, 1.0));
-    float axisX = smoothstep(0.0, fwidth(fragPos3D.z), abs(fragPos3D.z));
-    float axisZ = smoothstep(0.0, fwidth(fragPos3D.x), abs(fragPos3D.x));
+
+    // High at line fragments only
+    float lineOpacity = 1.0 - min(grid.x, grid.y);
+
+    // High everywhere except for its axis
+    float axisX = smoothstep(0.0, fwidth(worldPos.z), abs(worldPos.z));
+    float axisZ = smoothstep(0.0, fwidth(worldPos.x), abs(worldPos.x));
     vec3 base = vec3(0.2);
-    base.x = mix(1.0, base.x, axisZ);  // Red for X axis
-    base.z = mix(1.0, base.z, axisX);  // Blue for Z axis
+    base.x = mix(1.0, base.x, axisX);  // Red for X axis
+    base.z = mix(1.0, base.z, axisZ);  // Blue for Z axis
 
-    float alpha = 1.0 - min(line, 1.0);
-
-    return vec4(base, alpha);
+    return vec4(base, lineOpacity);
 }
 
 float computeDepth(vec3 pos) {
     vec4 clip_space_position = projection * view * vec4 (pos.xyz, 1.0);
+    // -1 -> 1 to 0 -> 1
     return 0.5 + 0.5 * (clip_space_position.z / clip_space_position.w);
 }
 
-float computeLinearDepth(vec3 pos) {
+float computeLinearDepth(float depth) {
     float near = 0.1;
     float far = 100.0;
-    vec4 clip_space_pos = projection * view * vec4(pos.xyz, 1.0);
-    float clip_space_depth = (clip_space_pos.z / clip_space_pos.w) * 0.5 + 0.5; // put back between -1 and 1
-    float linearDepth = (2.0 * near * far) / (far + near - clip_space_depth * (far - near)); // get linear value between 0.01 and 100
-    return linearDepth / far; // normalize
+    float linearDepth = (2.0 * near * far) / (far + near - depth * (far - near)); // get linear value between 0.01 and 100
+    return linearDepth / far; // normalize to near/far -> 1.0
 }
 
 void main() {
     // Only render on the floor -> y = 0
     float t = -nearPoint.y / (farPoint.y - nearPoint.y);
     if (t <= 0) discard;
-    vec3 xyPlanePos = nearPoint + t * (farPoint - nearPoint);
-    gl_FragDepth = computeDepth(xyPlanePos);
+    vec3 worldPos = nearPoint + t * (farPoint - nearPoint);
 
-    float linearDepth = computeLinearDepth(xyPlanePos);
+    float depth = computeDepth(worldPos);
+    gl_FragDepth = depth;
+
+    float linearDepth = computeLinearDepth(depth);
+    // Clamp to 0.5. Higher value -> less fading
     float fading = max(0, (0.5 - linearDepth));
 
-    FragColor = grid(xyPlanePos, 1, true); // adding multiple resolution for the grid
+    FragColor = grid(worldPos, 1, true);
     FragColor.a *= fading;
 };
