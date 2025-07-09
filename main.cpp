e35d9c58f5a758104e1e1d6665eca6e3eea3b283
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2025-07-09 22:39:32 +0300

    set minimum window size to 400x300

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char* argv[])
     Renderer* renderer = new Renderer(scene);
 
     MainWindow w(renderer);
-
+    w.setMinimumSize(400, 300);
     w.resize(1200, 900);
     w.show();
     return a.exec();
